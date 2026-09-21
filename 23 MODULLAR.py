df42ea900fc52ede1f41ece42f280ea9f456ba3f
shuxrat90 <shuxratarifdjanov@gmail.com>
2026-09-21 10:06:43 +0500

    1

---
+++ 23 MODULLAR.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 15 18:22:00 2026
+
+@author: ANVAR
+"""
+
+# import math
+
+# x=400
+# print(math.sqrt(x))
+# print(math.pow(5,2))
+# print(math.pi)
+# print(math.log2(8))
+# print(math.log10(100))
+# print(math.log(125, 5))
+
+
+import random as r
+
+# #randint()
+# son = r.randint(10,50)
+# print(son)
+
+# choice()
+# ismlar = ['olim', 'anvar', 'hasan', 'husan']
+# ism = r.choice(ismlar)
+# print(ism)
+# print(r.choice(ism))
+
+# x = list(range(0,51,5))
+# print(x)
+# print(r.choice(x))
+
+# shuffle()
+x = list(range(11))
+print(x)
+r.shuffle(x)
+print(x)
---
+++ main.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 15 18:22:00 2026
+
+@author: ANVAR
+"""
+
+# import avto_info_mod
+
+# avto1 = avto_info_mod.avto_info("GM", "Malibu", "Qora", "Avtomat", 2020, 40000)
+# avto_info_mod.info_print(avto1)
+
+
+
+# import avto_info_mod as aim
+
+# avto1 = aim.avto_info("GM", "Malibu", "Qora", "Avtomat", 2020, 40000)
+# aim.info_print(avto1)
+
+
+
+# from avto_info_mod import avto_info, info_print 
+
+# avto1 = avto_info("GM", "Malibu", "Qora", "Avtomat", 2020, 40000)
+# info_print(avto1)
+
+
+
+# from avto_info_mod import avto_info as ainfo, info_print as iprint 
+
+# avto1 = ainfo("GM", "Malibu", "Qora", "Avtomat", 2020, 40000)
+# iprint(avto1)
+
+
+
+from avto_info_mod import *
+
+avto1 = avto_info("GM", "Malibu", "Qora", "Avtomat", 2020, 40000)
+info_print(avto1)
